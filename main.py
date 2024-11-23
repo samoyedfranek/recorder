596a03c7b2d4cf70c766381676d0bf9c02636bc1
Franek <fhadzlik9@gmail.com>
2024-11-23 17:18:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -108,13 +108,8 @@ def monitor_and_record(com_port, input_device_id):
         if serial_connection:
             record(serial_connection)
         else:
-            print("Failed to initialize serial connection.")
-    
-    except serial.SerialException as e:
-        print(f"Error opening serial port {com_port}: {e}, switching to normal mode...")
-        record("radio")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+            print(f"Error opening serial port {com_port}: {e}, switching to normal mode...")
+            record("radio")
 
 def main():
     # Load saved configuration if available
