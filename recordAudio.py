658b70f6f8c5cf636bf6c27df1d43394b6fa85a4
Fryncu <fhadzlik9@gmail.com>
2025-01-04 19:22:36 +0100

    Update recordAudio.py

--- recordAudio.py
+++ recordAudio.py
@@ -16,7 +16,7 @@ def record():
     input_device_id = config["input_device"]
     com_port = config["com_port"]
 
-    CHUNK = 4096
+    CHUNK = 8192
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 48000
