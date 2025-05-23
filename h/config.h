2c171ebae2aaecfee25a493b38e141746dbc199c
Fryncu <fhadzlik9@gmail.com>
2025-05-23 21:44:07 +0200

    a

--- h/config.h
+++ h/config.h
@@ -1,30 +1,12 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define MAX_LINE_LENGTH 512
-#define MAX_ENV_VARS 20
-#define MAX_CHAT_IDS 10
+extern char *BOT_TOKEN;
+extern char *COM_PORT;
+extern char *RECORDING_DIRECTORY;
+extern int AUDIO_INPUT_DEVICE;
+extern char **CHAT_IDS;
 
-typedef struct
-{
-    char BOT_TOKEN[256];
-    char *CHAT_IDS[MAX_CHAT_IDS]; // NULL-terminated array
-    char COM_PORT[256];
-    char RECORDING_DIRECTORY[256];
-    int AUDIO_INPUT_DEVICE;
-    int AUDIO_OUTPUT_DEVICE;
-    char USER_NAME[256];
-    char WORKDIR[256];
-    char RECORDER_CMD[256];
-    char REPO_BRANCH[64];
-    int AMPLITUDE_THRESHOLD;
-    int DEBUG_AMPLITUDE;
-    int LIVE_LISTEN;
-    char EXTRA_TEXT[256];
-} CONFIG;
+void load_config(const char *env_file);
 
-extern CONFIG config;
-
-int load_config(const char *filename);
-
-#endif
+#endif // CONFIG_H
