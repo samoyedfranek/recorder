645e4240fbeba118f5604e99c52cda2edecd4f19
Fryncu <fhadzlik9@gmail.com>
2025-05-25 16:54:49 +0200

    Update recordAudio.c

--- recordAudio.c
+++ recordAudio.c
@@ -189,7 +189,7 @@ void recorder(const char *com_port)
             Pa_Terminate();
             return;
         }
-        outputParams.channelCount = 2;
+        outputParams.channelCount = CHANNELS;
         outputParams.sampleFormat = paInt16;
         outputParams.suggestedLatency = Pa_GetDeviceInfo(outputParams.device)->defaultLowOutputLatency;
         outputParams.hostApiSpecificStreamInfo = NULL;
