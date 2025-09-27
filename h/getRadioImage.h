aad6ea1167d518cb4f0e1e53082bb9eb241e3b2f
Fryncu <fhadzlik9@gmail.com>
2025-09-27 12:05:08 +0200

    Update getRadioImage.h

---
+++ h/getRadioImage.h
@@ -0,0 +1,13 @@
+#ifndef GETRADIOIMAGE_H
+#define GETRADIOIMAGE_H
+
+#include <stdint.h>
+
+int radio_init(const char *port);
+
+void radio_update(void);
+
+void radio_close(void);
+
+
+#endif // GETRADIOIMAGE_H
