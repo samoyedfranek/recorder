1520b3c350db73647365da0f6a2c148f8f566ee7
Fryncu <fhadzlik9@gmail.com>
2025-09-27 11:51:40 +0200

    Update telegramSend.h

--- h/telegramSend.h
+++ h/telegramSend.h
@@ -5,6 +5,6 @@
 
 void get_current_datetime(char *datetime_str, size_t size);
 
-int send_to_telegram(const char *file_path, const char *bot_token, char **chat_ids);
+int send_to_telegram(const char *file_path, const char *bot_token, char **chat_ids, const char *image_path);
 int send_telegram_status(const char *bot_token, char **chat_ids, const char *message);
 #endif
